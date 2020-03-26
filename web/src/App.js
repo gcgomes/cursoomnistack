d43b9d011cb7d5d95c5a83b119de952dff5243be
Gabriel Gomes <gabrielcesariogomes@gmail.com>
2020-03-26 17:12:30 -0300

    Aprendendo conceitos React

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from "./Header";
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-      <h1>Hello World</h1>
+    <div>
+      <Header>{counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
