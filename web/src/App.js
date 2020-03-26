7f4d0ddd4e99e22f83f7cc8e0c32dc823ff690c9
Gabriel Gomes <gabrielcesariogomes@gmail.com>
2020-03-26 19:46:13 -0300

    Finalização do design frontend web

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
-import Header from "./Header";
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes'
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>{counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+      <Routes />
   );
 }
 
